8f02158100c2da12fa8a5394afe09ea02aca34a1
t20x <mpx@gmail.com>
2020-07-16 09:38:01 +0100

    asdf

--- ubuntu/entrypoint.sh
+++ ubuntu/entrypoint.sh
@@ -1,4 +1,6 @@
 #git config --global credential.helper store
 trap "exit 1" TERM
 sudo mknod /dev/null c 1 3
+mkdir /tmp/cores
+sudo sysctl -w /tmp/cores/kernel.core_pattern=core.%e.%p 
 tail -f /dev/null
