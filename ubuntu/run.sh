fd677e091354a23da3d358a684ef51a86b65bb67
doesnotmatter <you@example.com>
2021-03-25 11:53:50 +0000

    fix the build

--- ubuntu/run.sh
+++ ubuntu/run.sh
@@ -1,1 +1,1 @@
-docker run -d -p 8000:8000 -p 8001:8001 --privileged --net=host --name ubuntu-docker $1
+docker run -d -p 8000:8000 -p 8001:8001 -p 8002:8002 -p 8003:8003 --privileged --net=host --name dev-container $1
